af5ee2dcdc0b9c3c12508a4aa6e2979d3246c5b3
Murilotf <murilotf.m4@gmail.com>
2018-07-19 00:35:45 -0300

    creating a super game

--- app.js
+++ app.js
@@ -4,5 +4,42 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false
+    },
+    methods: { 
+    startGame: function(){
+        this.gameIsRunning = true;
+        this.playerHealth = 100;
+        this.monsterHealth = 100;
+    },
+    attack: function(){
+        this.monsterHealth -= this.calculateDamage(10 , 3);        
+        if(this.checkWin()){
+           return;
+        }
+        this.playerHealth -= this.calculateDamage(12 , 5);
+        this.checkWin();
+    },
+    calculateDamage: function(maxDamage,minDamage){
+        return Math.max(Math.floor(Math.random()*maxDamage)+1,minDamage);
+    },
+    checkWin: function(){
+        if(this.monsterHealth <= 0 ){
+            if(confirm('You won! Another Game?')){
+                this.startGame();            
+            }else{
+                this.gameIsRunning = false;
+            }
+            return true;
+        }
+        else if(this.playerHealth <= 0){
+            if(confirm('You lost! Another Game?')){
+                this.startGame();
+            }else{
+                this.gameIsRunning = false;
+            }
+            return true;
+        }
+        return false;
     }
+}
 });
